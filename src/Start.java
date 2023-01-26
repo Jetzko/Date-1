5ec6c0e5d0348a054babfb68da3bfd9edca1c95d
Jetzko <andreadefilippi68@gmail.com>
2023-01-26 11:42:23 +0100

    Date 1

--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 public class Start {
     public static void main(String[] args) {
         LocalDate todayDate = LocalDate.now();
-        System.out.println("Days to end of this month: " + ((todayDate.getDayOfMonth()) - (todayDate.lengthOfMonth())));
+        System.out.println("Days to end of this month: " + ((todayDate.lengthOfMonth()) - (todayDate.getDayOfMonth())));
         System.out.println("Days to end of this year: " + ((todayDate.lengthOfYear()) - (todayDate.getDayOfYear())));
     }
 }
